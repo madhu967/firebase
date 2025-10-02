fcfbc7aa5be574d735d40a577e639e52fe452c26
madhu967 <156585768+madhu967@users.noreply.github.com>
2025-10-02 19:03:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,37 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import { FirebaseProvider } from "./context/Firebase";
+import { messaging } from './firebase';
+import { getToken,onMessage  } from 'firebase/messaging';
 
 function App() {
+
+   async function requestPermission(){
+    const permission =await Notification.requestPermission();
+    if(permission === 'granted'){
+      //Generate Token
+      const token = await getToken(messaging,{vapidKey: 'BIQkjtnYvNAyK_nZAk9UNI6DdEQfyAb8FON3-7BPOAm9GaMT2nsD1M7Wqg99bH5U4abO-cHtXkSDzmSg2czfdWw'});
+      console.log("Token Gen ",token );
+    }
+    else if (permission === 'denied'){
+      alert("You denied for the notifications");
+    }
+
+  }
+
+  useEffect(() => {
+    requestPermission();
+
+    // Foreground notifications
+    onMessage(messaging, (payload) => {
+      console.log("Foreground message:", payload);
+      new Notification(payload.notification.title, {
+        body: payload.notification.body,
+      });
+    });
+  }, []);
+  
+  
   return (
     <FirebaseProvider>
       <Router>
